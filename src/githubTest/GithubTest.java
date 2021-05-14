b117d62573a92939a1143d61b590254ad8cbab6d
sanggi1 <rlatkdrl321@gmail.com>
2021-05-14 11:42:37 +0900

    첫번째 커밋

---
+++ src/githubTest/GithubTest.java
@@ -0,0 +1,13 @@
+package githubTest;
+
+public class GithubTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("github test");
+	}
+
+}
